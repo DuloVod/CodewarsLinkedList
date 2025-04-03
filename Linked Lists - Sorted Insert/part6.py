5b652e680236f3f8a6d7d8fa21152ff1300ddd93
DuloVod <vladv@MacBook-Air-Vlad.local>
2025-04-03 13:15:52 +0300

    part6

---
+++ Linked Lists - Sorted Insert/part6.py
@@ -0,0 +1,21 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def sorted_insert(head, data):
+    new_node = Node(data)
+    if not head or data <= head.data:
+        new_node.next = head
+        return new_node
+
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+
+    # Insert the new node
+    new_node.next = current.next
+    current.next = new_node
+
+    return head
